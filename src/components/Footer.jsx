3ec1ac98a000d696f7ccfcf0f9ed0e7707bffb0b
RageNeko26 <rneko2006@gmail.com>
2023-05-31 23:55:53 +0700

    Adding footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<div className="container mt-3 mx-2 p-5 flex justify-center items-center">
+			<h2 className="text-lg font-semibold">Copyright 2023 &copy; Gions Ilustrator</h2>
+		</div>
+	)
+}
+
+export default Footer
