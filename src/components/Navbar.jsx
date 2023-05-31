e9a2d61943568451eeb54ddf9d4d435fd7adec01
RageNeko26 <rneko2006@gmail.com>
2023-05-31 22:14:21 +0700

    Change App title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = (props) => {
 
     return (
         <div className="container  w-screen bg-white shadow">
-            <div className="flex w-screen mx-2 p-3 text-xl max-w-full font-bold justify-between ">
-                <h2>Gions Freekley</h2>
+            <div className="flex w-screen mx-2 p-3 max-w-full justify-between ">
+                <h2 className="text-xl font-bold">Gions Ilustrator</h2>
                 <div onClick={handler}>
                     <span className={hamburger}></span>
                     <span className={hamburger}></span>
