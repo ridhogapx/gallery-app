64a2cf18fb9622cdea2da5cc80ba527dd8ba42e7
RageNeko26 <rneko2006@gmail.com>
2023-06-01 23:43:03 +0700

    Clean up in Ilustration component

--- src/components/Ilustration.jsx
+++ src/components/Ilustration.jsx
@@ -1,25 +1,25 @@
+import Section from "./Section";
 import first from "../assets/ilustration/1.jpg"
 import second from "../assets/ilustration/2.jpg"
 import third from "../assets/ilustration/3.jpg"
 
 const Ilustration = () => {
+    const content = [
+        {
+            id: 1,
+            src: first
+        },
+         {
+            id: 2,
+            src: second
+        }, {
+            id: 3,
+            src: third
+        },
+    ]
+
     return(
-    	<div className="container mx-2 mt-3 p-3">
-    		<div className="max-w-full">
-    			<h2 className="text-lg w-fit lg:mx-5 font-semibold border-b-2 border-lime-600">Ilustration</h2>
-    		</div>
-    		<div className="max-w-full flex flex-wrap lg:mx-5 ">
-    			<div className="max-w-sm max-h-72 mt-2 md:mx-2 overflow-hidden">
-    				<img src={first} alt="Content" className="w-72 lg:w-96" />
-    			</div>
-    			<div className="max-w-sm max-h-72 mt-2 md:mx-2 overflow-hidden ">
-    				<img src={second} alt="Content" className="w-72 lg:w-96" />
-    			</div>
-    			<div className="max-w-sm max-h-72 mt-2 md:mx-2 overflow-hidden ">
-    				<img src={third} alt="Content" className="w-72 lg:w-96" />
-    			</div>
-    		</div>
-    	</div>
+    	<Section title="Ilustration" content={content} />
     )
 }
 
