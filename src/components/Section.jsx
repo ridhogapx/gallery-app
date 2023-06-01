bed630944087b0accfdbe415cf1d3eb2c00b4b05
RageNeko26 <rneko2006@gmail.com>
2023-06-01 23:05:41 +0700

    Add single section

---
+++ src/components/Section.jsx
@@ -0,0 +1,18 @@
+const Section = (props) => {
+	const { content } = props
+	const { title } = props
+	return (
+		<div className="container mx-2 mt-3 p-3">
+			<div className="max-w-full">
+				<h2 className="text-lg w-fit lg:mx-5 font-semibold border-b-2 border-yellow-600">{title}</h2>
+			</div>
+			<div className="max-w-full flex flex-wrap lg:mx-5 ">
+			{content.map((result) => 
+				<div className="max-w-sm max-h-72 md:mx-2 overflow-hidden mt-2 md:w-72">
+					<img src={result.src} alt="Content" className="w-72 lg:w-96" />
+				</div>
+				)}
+			</div>
+		</div>
+	)
+}
