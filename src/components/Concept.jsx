99a73b04dcdcda1effc3fc3bf898794673eb0d4a
RageNeko26 <rneko2006@gmail.com>
2023-06-01 13:55:40 +0700

    Remove justify center

--- src/components/Concept.jsx
+++ src/components/Concept.jsx
@@ -9,10 +9,10 @@ const Concept = () => {
     return (
         <div className="container mx-2 p-3 pt-16">
             <div className="max-w-full">
-                <h2 className="text-lg w-fit lg:mx-10 font-semibold border-b-2 border-red-600 ">Concept</h2>
+                <h2 className="text-lg w-fit  font-semibold border-b-2 border-red-600 ">Concept</h2>
             </div>
 
-            <div className="max-w-full flex flex-wrap lg:justify-center">
+            <div className="max-w-full flex flex-wrap">
                 <div className="max-w-sm max-h-72 mt-2 md:mx-2  overflow-hidden">
                     <img src={first} alt="Content" className="w-72 lg:w-96" />
                 </div>
--- src/components/Ilustration.jsx
+++ src/components/Ilustration.jsx
@@ -6,9 +6,9 @@ const Ilustration = () => {
     return(
     	<div className="container mx-2 mt-3 p-3">
     		<div className="max-w-full">
-    			<h2 className="text-lg w-fit lg:mx-10 font-semibold border-b-2 border-lime-600">Ilustration</h2>
+    			<h2 className="text-lg w-fit font-semibold border-b-2 border-lime-600">Ilustration</h2>
     		</div>
-    		<div className="max-w-full flex flex-wrap lg:justify-center">
+    		<div className="max-w-full flex flex-wrap ">
     			<div className="max-w-sm max-h-72 mt-2 md:mx-2 overflow-hidden">
     				<img src={first} alt="Content" className="w-72 lg:w-96" />
     			</div>
