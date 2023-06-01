96cc7b2969f7c61724440e33386e3bc49a684403
RageNeko26 <rneko2006@gmail.com>
2023-06-01 13:46:48 +0700

    Responsive in all section components

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -6,18 +6,18 @@ import third from "../assets/personal/3.jpg"
 const Personal = () => {
 	return (
 		<div className="container mx-2 mt-3 p-3">
-			<div className="w-full">
-				<h2 className="text-lg w-fit font-semibold border-b-2 border-yellow-600">Personal</h2>
+			<div className="max-w-full">
+				<h2 className="text-lg w-fit lg:mx-10 font-semibold border-b-2 border-yellow-600">Personal</h2>
 			</div>
-			<div className="w-screen flex flex-wrap md:justify-center">
-				<div className="w-3/4 mt-2 md:w-72">
-					<img src={first} alt="Content" />
+			<div className="max-w-full flex flex-wrap lg:justify-center">
+				<div className="max-w-sm max-h-72 md:mx-2 overflow-hidden mt-2 md:w-72">
+					<img src={first} alt="Content" className="w-72 lg:w-96" />
 				</div>
-				<div className="w-3/4 mt-2 md:w-72">
-					<img src={second} alt="Content" />
+				<div className="max-w-sm max-h-72 md:mx-2 overflow-hidden mt-2 md:w-72">
+					<img src={second} alt="Content" className="w-72 lg:w-96" />
 				</div>
-				<div className="w-3/4 mt-2 md:w-72">
-					<img src={third} alt="Content" />
+				<div className="max-w-sm max-h-72 md:mx-2 overflow-hidden mt-2 md:w-72">
+					<img src={third} alt="Content" className="w-72 lg:w-96" />
 				</div>
 			</div>
 		</div>
