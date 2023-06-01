96993078d70053fe9796ca2ab32c6c54a968152b
RageNeko26 <rneko2006@gmail.com>
2023-06-01 23:45:49 +0700

    Clean up in Personal Component

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -1,26 +1,27 @@
+import Section from "./Section"
 import first from "../assets/personal/1.jpg"
 import second from "../assets/personal/2.jpg"
 import third from "../assets/personal/3.jpg"
 
 
 const Personal = () => {
+	const content = [
+		{
+			id: 1,
+			src: first
+		},
+		{
+			id: 2,
+			src: second
+		},
+		{
+			id: 3,
+			src: third
+		},
+	]
+
 	return (
-		<div className="container mx-2 mt-3 p-3">
-			<div className="max-w-full">
-				<h2 className="text-lg w-fit lg:mx-5 font-semibold border-b-2 border-yellow-600">Personal</h2>
-			</div>
-			<div className="max-w-full flex flex-wrap lg:mx-5 ">
-				<div className="max-w-sm max-h-72 md:mx-2 overflow-hidden mt-2 md:w-72">
-					<img src={first} alt="Content" className="w-72 lg:w-96" />
-				</div>
-				<div className="max-w-sm max-h-72 md:mx-2 overflow-hidden mt-2 md:w-72">
-					<img src={second} alt="Content" className="w-72 lg:w-96" />
-				</div>
-				<div className="max-w-sm max-h-72 md:mx-2 overflow-hidden mt-2 md:w-72">
-					<img src={third} alt="Content" className="w-72 lg:w-96" />
-				</div>
-			</div>
-		</div>
+		<Section title="Personal" content={content} />
 	)
 }
 
