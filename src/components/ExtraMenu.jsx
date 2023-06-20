4c2a6edaf452a1714f6d36243435de93c1526ab9
RageNeko26 <rneko2006@gmail.com>
2023-06-20 14:23:46 +0700

    feat: Active router styling

--- src/components/ExtraMenu.jsx
+++ src/components/ExtraMenu.jsx
@@ -5,23 +5,22 @@ const ExtraMenu = (props) => {
     const { styleLink} = props;
 
     return (
-        <nav className={styleMenu}>
-             <div>
+        <nav className={styleMenu} id="mobileNav">
+             <div className="mNav">
                 <NavLink to="/" className={styleLink}>Home</NavLink>
             </div>
-            <div>
+            <div className="mNav">
                 <NavLink to="/concept" className={styleLink}>Concept</NavLink>
             </div>
-            <div>
+            <div className="mNav">
                 <NavLink to="" className={styleLink}>Ilustration</NavLink>
             </div>
-            <div>
+            <div className="mNav">
                 <NavLink to="" className={styleLink}>Personal Project</NavLink>
             </div>
-            <div>
+            <div className="mNav">
                 <NavLink to="" className={styleLink}>Contact</NavLink>
             </div>
-           
         </nav>
     )
 }
