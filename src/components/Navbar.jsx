a82437a3bbb765a1893dda45767fb645f5c6ec5c
RageNeko26 <rneko2006@gmail.com>
2023-06-20 13:38:49 +0700

    fix: Router

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,8 @@ const Navbar = (props) => {
                     <span className={hamburger}></span>
                 </div>
                 <div className="hidden lg:flex">
-                    <NavLink to="/concept" className="block mx-3 border-b-2 border-slate-700">Concept</NavLink>
+                    <NavLink to="/" className="block mx-3">Home</NavLink>
+                    <NavLink to="/concept" className="block mx-3">Concept</NavLink>
                     <NavLink to="" className="block mx-3">Ilustration</NavLink>
                     <NavLink to="" className="block mx-3">Personal Project</NavLink>
                     <NavLink to="" className="block mx-3">Contact</NavLink>
