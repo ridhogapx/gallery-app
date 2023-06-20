4e890290308e26950d39226bf72ef7a910599d60
RageNeko26 <rneko2006@gmail.com>
2023-06-20 13:35:46 +0700

    refactor: change nav link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = (props) => {
                 </div>
                 <div className="hidden lg:flex">
                     <NavLink to="/concept" className="block mx-3 border-b-2 border-slate-700">Concept</NavLink>
-                    <a href="# "className="block mx-3">Ilustration</a>
+                    <NavLink to="" className="block mx-3">Ilustration</NavLink>
                     <NavLink to="" className="block mx-3">Personal Project</NavLink>
-                    <a href="#" className="block mx-3">Contact</a>
+                    <NavLink to="" className="block mx-3">Contact</NavLink>
                 </div>
             </div>
         </div>
