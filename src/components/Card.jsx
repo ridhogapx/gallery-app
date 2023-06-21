28d0063006626b3d0e618567c1e33587b84cfe89
RageNeko26 <rneko2006@gmail.com>
2023-06-22 00:22:54 +0700

    fix: remove unused state

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,8 @@
 const Card = (props) => {
-    const { children, title, optional } = props 
-    const style = ["m-5", "border-2", "p-5", "border-zinc-100", "rounded-lg", "lg:w-1/2"]
-
-    if(optional !== null) {
-        style.push(optional)
-    }
-
+    const { children, title } = props 
+   
     return (
-        <div className={style.join(" ")}>
+        <div className="m-5 border-2 p-5 border-zinc-100 rounded-lg lg:w-1/2">
            <h2 className="text-lg font-semibold lg:text-xl">{ title }</h2>
            {children}
         </div>
