89a25cdd1705814394a92569a86eb1927d8469a5
RageNeko26 <rneko2006@gmail.com>
2023-06-22 00:16:38 +0700

    feat: Implementing card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,9 @@
 const Card = (props) => {
-    const { children, title } = props 
+    const { children, title, optional } = props 
+    const style = ["m-5", "border-2", "p-5", "border-zinc-100", "rounded-lg", "lg:w-1/2"]
 
     return (
-        <div className="m-5 border-2 p-5 border-zinc-100 rounded-lg lg:w-1/2">
+        <div className={style.join(" ")}>
            <h2 className="text-lg font-semibold lg:text-xl">{ title }</h2>
            {children}
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,15 @@
+import Card from "./Card"
 const Hero = () => {
     return(
         <div className="container lg:mx-5 pt-20 flex">
-						<div className="m-5 border-2 p-5 border-zinc-100 rounded-lg lg:w-1/2">
-							<h2 className="text-lg font-semibold lg:text-xl">About Me</h2>
+					<Card title="About Me" optional={null} children={
+						<>
 							<p className="text-base mt-2 text-slate-400">
 								Hi! My name is Gions Freekley. Passionate student who love 2D and 3D things, such as Animation, Ilustration, Character Design, and 3D Modelling. I like to keep learning those things as i want to improve my own arts! 
 							</p>
 							<button className="mt-2 px-10 py-3 rounded-2xl bg-gradient-to-r from-cyan-500 to-blue-500 text-white hover:opacity-70 transition ease-in-out text-base">Contact</button>
-					</div>
+						</>
+					} />
 				</div>
     )
 }
