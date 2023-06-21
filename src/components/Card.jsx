48290ae0c8b3fc1e6124ae55ff46c5608efd7591
RageNeko26 <rneko2006@gmail.com>
2023-06-22 00:18:41 +0700

    feat: Optional parameter

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,10 @@ const Card = (props) => {
     const { children, title, optional } = props 
     const style = ["m-5", "border-2", "p-5", "border-zinc-100", "rounded-lg", "lg:w-1/2"]
 
+    if(optional !== null) {
+        style.push(optional)
+    }
+
     return (
         <div className={style.join(" ")}>
            <h2 className="text-lg font-semibold lg:text-xl">{ title }</h2>
