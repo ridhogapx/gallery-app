f027d3e9c32e6f47e9e95d6141d4e20f171b899f
RageNeko26 <rneko2006@gmail.com>
2023-06-21 23:43:55 +0700

    fix: Remove bg color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,12 @@
 const Hero = () => {
     return(
         <div className="container lg:mx-5 pt-20">
-					<div className="h-52 lg:h-56 rounded-lg mx-3 p-5 lg:mx-5 bg-white blur-sm"></div>
-					<div className="max-w-full h-52 lg:h-96 flex absolute p-5 top-[10vh] z-10">
-						<div className="m-5 ">
+						<div className="m-5 border-2 p-5 border-zinc-100 rounded-lg">
 							<h2 className="text-xl font-semibold lg:text-xl">About Me</h2>
 							<p className="text-lg mt-2 text-slate-400">
 								Hi! My name is Gions Freekley. Passionate student who love 2D and 3D things, such as Animation, Ilustration, Character Design, and 3D Modelling. I like to keep learning those things as i want to improve my own arts! 
 							</p>
-							<p className="text-lg mt-2 text-slate-400">Currently, i'm opening commision. You can contact me through platform listed below: </p>
-						</div>
+							<p className="text-lg mt-2 text-slate-400">Currently, i'm opening commision. You can contact me throught below button:  </p>
 					</div>
 				</div>
     )
