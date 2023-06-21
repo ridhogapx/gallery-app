039c1b98be8259542f58989dd4c464e34620b583
RageNeko26 <rneko2006@gmail.com>
2023-06-21 23:00:56 +0700

    fix: change bg color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 const Hero = () => {
     return(
         <div className="container lg:mx-5 pt-20">
-					<div className="h-52 lg:h-56 rounded-lg mx-3 p-5 lg:mx-5 bg-slate-100 blur-sm"></div>
+					<div className="h-52 lg:h-56 rounded-lg mx-3 p-5 lg:mx-5 bg-slate-500 blur-sm"></div>
 					<div className="max-w-full h-52 lg:h-96 flex absolute p-5 top-[10vh] z-10">
 						<div className="m-5 ">
 							<h2 className="text-lg font-semibold lg:text-xl">About Me</h2>
