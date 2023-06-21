3afeed43076c5ae3b4947b022e56b6f99ec541ff
RageNeko26 <rneko2006@gmail.com>
2023-06-22 00:09:32 +0700

    fix: adding Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+const Card = (props) => {
+    const { children, title } = props 
+
+    return (
+        <div className="m-5 border-2 p-5 border-zinc-100 rounded-lg lg:w-1/2">
+           <h2 className="text-lg font-semibold lg:text-xl">{ title }</h2>
+           {children}
+        </div>
+    )
+}
+
+export default Card
