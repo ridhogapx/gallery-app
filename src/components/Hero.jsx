2a400b1389f8e3d0dfd1969d083c68827bbfd086
RageNeko26 <rneko2006@gmail.com>
2023-06-22 00:49:20 +0700

    feat: Improve stats component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,12 +12,10 @@ const Hero = () => {
 						</>
 					} />
 					<Card title="Skills" children={
-						<div className="mt-2">
-							<h2 className="text-slate-400 text-base">3D Modelling</h2>
-							<Stats width="w-[65%]" />
-							<h2 className="mt-1 text-slate-400 text-base">Animation</h2>
-							<Stats width="w-[54%]"/>
-						</div>
+						<>
+							<Stats title="3D Modelling" width="w-[65%]" />
+							<Stats title="Photoshop" width="w-[54%]"/>
+						</>
 					} />
 				</div>
     )
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,11 +1,14 @@
 const Stats = (props) => {
-    const { width } = props
+    const { width, title } = props
     const style = ["h-1", "p-2", "rounded-2xl", "bg-gradient-to-r", "from-cyan-500", "to-blue-500"]
     
     style.push(width)
     
     return (
-        <div className={style.join(" ")}></div>
+        <>
+            <h2 className="mt-1 text-slate-400 text-base">{ title }</h2>
+            <div className={style.join(" ")}></div>
+        </>
     )
 }
 
