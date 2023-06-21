bd82d3ed81d54b1888d3542d3ed8d2939e183932
RageNeko26 <rneko2006@gmail.com>
2023-06-21 22:14:38 +0700

    feat: Adding icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import icon from "../assets/50.png"
 
 const Navbar = (props) => {
     const { handler } = props;
@@ -7,7 +8,8 @@ const Navbar = (props) => {
     return (
         <div className="container fixed top-0 z-10 backdrop-blur-md w-screen">
             <div className="flex w-screen mx-2 p-3 max-w-full justify-between ">
-                <h2 className="text-xl lg:mx-5 font-bold">Gions Ilustrator</h2>
+                {/* <h2 className="text-xl lg:mx-5 font-bold">Gions Ilustrator</h2> */}
+                <img src={icon} alt="Icon" className="mx-5" />
                 {/* Hamburger */}
                 <div onClick={handler} className="md:hidden">
                     <span className={hamburger}></span>
