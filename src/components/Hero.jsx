b35d865236649d2e8c4974dbaab576f589ca6693
RageNeko26 <rneko2006@gmail.com>
2023-06-21 23:58:50 +0700

    feat: adding contact button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,6 +7,7 @@ const Hero = () => {
 								Hi! My name is Gions Freekley. Passionate student who love 2D and 3D things, such as Animation, Ilustration, Character Design, and 3D Modelling. I like to keep learning those things as i want to improve my own arts! 
 							</p>
 							<p className="text-lg mt-2 text-slate-400">You can contact me throught below button:  </p>
+							<button className="mt-2 px-10 py-3 rounded-2xl bg-gradient-to-r from-cyan-500 to-blue-500 text-white hover:opacity-70 transition ease-in-out">Contact</button>
 					</div>
 				</div>
     )
