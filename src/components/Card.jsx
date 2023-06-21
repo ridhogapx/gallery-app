3006eae380131e6a86ad26a55ebb8ac166c0d80c
RageNeko26 <rneko2006@gmail.com>
2023-06-22 00:56:48 +0700

    fix: Width in mobile

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ const Card = (props) => {
     const { children, title } = props 
    
     return (
-        <div className="m-5 border-2 p-5 border-zinc-100 rounded-lg lg:w-1/2">
+        <div className="w-full m-5 border-2 p-5 border-zinc-100 rounded-lg lg:w-1/2">
            <h2 className="text-lg font-semibold lg:text-xl">{ title }</h2>
            {children}
         </div>
