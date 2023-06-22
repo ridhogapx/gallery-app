8482f85ffc023d8472c3db9b1cd190ed87b703dc
RageNeko26 <rneko2006@gmail.com>
2023-06-22 18:14:48 +0700

    fix: Change icon sizing

--- src/components/Sosmed.jsx
+++ src/components/Sosmed.jsx
@@ -1,7 +1,7 @@
 const Sosmed = () => {
     return (
-        <div className="flex mt-2">
-            <a href="#">
+        <div className="w-full flex mt-2">
+            <a href="#" className="p-2 text-6xl">
                 <i className="bi bi-envelope"></i>
             </a>
         </div>
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -4,7 +4,7 @@ const ContactPage = () => {
     return (
         <Transition children={
             <div className="container max-w-full pt-20 flex justify-center ">
-                <div className="w-1/3 p-5 bg-white bg-opacity-20 shadow-lg rounded-2xl border border-zinc-100">
+                <div className="lg:w-1/3 p-5 bg-white bg-opacity-20 shadow-lg rounded-2xl border border-zinc-100">
                     <h2 className="font-bold text-lg text-center">Contact</h2>
                     <p className="text-slate-400 mt-2 text-base">Any Question? Collaboration? and Commision? Contact me throught below platform</p>
                     <Sosmed />
