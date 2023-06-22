d32826d53ac82ccde3b2ba1b5b76533a9c1bda55
RageNeko26 <rneko2006@gmail.com>
2023-06-22 13:00:37 +0700

    feat: Adding content in Concept page

--- src/pages/ConceptPage.jsx
+++ src/pages/ConceptPage.jsx
@@ -1,10 +1,11 @@
 import Transition from "../components/Transition"
+import Concept from "../components/Concept"
 
 const ConceptPage = () => {
     return (
        <Transition children={
-        <div className="pt-14">
-            <h2>Concept Page!</h2>
+        <div className="pt-5">
+            <Concept />
         </div>
        }/>
        
