630cdb534c8f01e145183d38fd7b16d5b291dc4f
RageNeko26 <rneko2006@gmail.com>
2023-06-22 18:20:21 +0700

    feat: Adding social media icons

--- src/components/Sosmed.jsx
+++ src/components/Sosmed.jsx
@@ -1,8 +1,17 @@
 const Sosmed = () => {
     return (
-        <div className="w-full flex mt-2">
-            <a href="#" className="p-2 text-6xl">
+        <div className="w-full mt-2 flex justify-center">
+            <a href="#" className="p-2 text-6xl mr-2">
                 <i className="bi bi-envelope"></i>
+            </a>
+            <a href="#" className="p-2 text-6xl mr-2">
+                <i className="bi bi-instagram"></i>
+            </a>
+            <a href="#" className="p-2 text-6xl mr-2">
+                <i className="bi bi-facebook"></i>
+            </a>
+            <a href="#" className="p-2 text-6xl">
+                <i className="bi bi-twitter"></i>
             </a>
         </div>
     )
