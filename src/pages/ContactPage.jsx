3dc9b4166d10265719ec7bb3a4ec20796be2e9fa
RageNeko26 <rneko2006@gmail.com>
2023-06-22 16:23:01 +0700

    feat: adding content in Contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,8 +2,11 @@ import Transition from "../components/Transition"
 const ContactPage = () => {
     return (
         <Transition children={
-            <div className="pt-20">
-                <h2 className="text-lg">Contact</h2>
+            <div className="container max-w-full pt-20 flex justify-center ">
+                <div className="w-1/3 p-5 bg-white bg-opacity-20 shadow-lg rounded-2xl border border-zinc-100">
+                    <h2 className="font-bold text-lg text-center">Contact</h2>
+                    <p className="text-slate-400 mt-2 text-base">Any Question? Collaboration? and Commision? Contact me throught below platform</p>
+                </div>
             </div>
         }/>
     )
