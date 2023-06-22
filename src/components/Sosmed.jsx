c6905a20d65c90e6cea705fb4402840eae9eb5e4
RageNeko26 <rneko2006@gmail.com>
2023-06-22 17:45:50 +0700

    fix: typo class

--- src/components/Sosmed.jsx
+++ src/components/Sosmed.jsx
@@ -2,7 +2,7 @@ const Sosmed = () => {
     return (
         <div className="flex mt-2">
             <a href="#">
-                <i class="bi bi-envelope"></i>
+                <i className="bi bi-envelope"></i>
             </a>
         </div>
     )
