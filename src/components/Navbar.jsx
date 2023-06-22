bb90483f0df202cd9a5887507d09ab866eea4e10
RageNeko26 <rneko2006@gmail.com>
2023-06-22 13:03:43 +0700

    refactor: separate style using array

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import icon from "../assets/pls.png"
 const Navbar = (props) => {
     const { handler } = props;
     const { hamburger } = props;
+    const style = ["block", "mx-3", "transition", "ease-in-out"]
 
     return (
         <div className="container fixed top-0 z-10 backdrop-blur-md w-screen">
@@ -19,11 +20,11 @@ const Navbar = (props) => {
                     <span className={hamburger}></span>
                 </div>
                 <div className="hidden lg:flex navMenu transition ease-in-out">
-                    <NavLink to="/" className="block mx-3 transition ease-in-out">Home</NavLink>
-                    <NavLink to="/concept" className="block mx-3 transition ease-in-out">Concept</NavLink>
-                    <NavLink to="/ilustration" className="block mx-3 transition ease-in-out">Ilustration</NavLink>
-                    <NavLink to="/personal" className="block mx-3 transition ease-in-out">Personal Project</NavLink>
-                    <NavLink to="/contact" className="block mx-3 transition ease-in-out">Contact</NavLink>
+                    <NavLink to="/" className={style.join(" ")}>Home</NavLink>
+                    <NavLink to="/concept" className={style.join(" ")}>Concept</NavLink>
+                    <NavLink to="/ilustration" className={style.join(" ")}>Ilustration</NavLink>
+                    <NavLink to="/personal" className={style.join(" ")}>Personal Project</NavLink>
+                    <NavLink to="/contact" className={style.join(" ")}>Contact</NavLink>
                 </div>
             </div>
         </div>
