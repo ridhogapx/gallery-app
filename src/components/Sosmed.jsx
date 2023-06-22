6e940e9ce3672a3f8ec456de41b362c9e6a7a68f
RageNeko26 <rneko2006@gmail.com>
2023-06-22 18:22:24 +0700

    fix: responsive icon size

--- src/components/Sosmed.jsx
+++ src/components/Sosmed.jsx
@@ -1,16 +1,16 @@
 const Sosmed = () => {
     return (
         <div className="w-full mt-2 flex justify-center">
-            <a href="#" className="p-2 text-6xl mr-2">
+            <a href="#" className="p-2 text-3xl lg:text-6xl mr-2">
                 <i className="bi bi-envelope"></i>
             </a>
-            <a href="#" className="p-2 text-6xl mr-2">
+            <a href="#" className="p-2 text-3xl lg:text-6xl mr-2">
                 <i className="bi bi-instagram"></i>
             </a>
-            <a href="#" className="p-2 text-6xl mr-2">
+            <a href="#" className="p-2 text-3xl lg:text-6xl mr-2">
                 <i className="bi bi-facebook"></i>
             </a>
-            <a href="#" className="p-2 text-6xl">
+            <a href="#" className="p-2 text-3xl lg:text-6xl">
                 <i className="bi bi-twitter"></i>
             </a>
         </div>
