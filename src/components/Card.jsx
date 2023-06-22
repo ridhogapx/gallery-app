3ae519a621e0892688500bbc13fa875eff2dbb74
RageNeko26 <rneko2006@gmail.com>
2023-06-22 12:45:53 +0700

    feat: Change Background color in Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ const Card = (props) => {
     const { children, title } = props 
    
     return (
-        <div className="w-full m-4 border-2 p-5 border-zinc-100 rounded-lg lg:w-1/3">
+        <div className="w-full m-4 border-2 p-5 border-zinc-100 rounded-lg lg:w-1/3 bg-white shadow-lg bg-opacity-30">
            <h2 className="text-lg font-semibold lg:text-xl">{ title }</h2>
            {children}
         </div>
