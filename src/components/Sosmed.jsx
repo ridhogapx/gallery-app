7bb2b745c9074de3cd526671ddb8779fe28e9554
RageNeko26 <rneko2006@gmail.com>
2023-06-23 12:39:24 +0700

    fix: Add active sosmed

--- src/components/Sosmed.jsx
+++ src/components/Sosmed.jsx
@@ -1,18 +1,13 @@
 const Sosmed = () => {
     return (
         <div className="w-full mt-2 flex justify-center">
-            <a href="#" className="p-2 text-3xl lg:text-6xl mr-2">
+            <a href="mailto:gionsfreekley814@gmail.com" target="_blank" className="p-2 text-3xl lg:text-6xl mr-2">
                 <i className="bi bi-envelope"></i>
             </a>
-            <a href="#" className="p-2 text-3xl lg:text-6xl mr-2">
+            <a href="https://www.instagram.com/gions_ilust" target="_blank" className="p-2 text-3xl lg:text-6xl mr-2">
                 <i className="bi bi-instagram"></i>
             </a>
-            <a href="#" className="p-2 text-3xl lg:text-6xl mr-2">
-                <i className="bi bi-facebook"></i>
-            </a>
-            <a href="#" className="p-2 text-3xl lg:text-6xl">
-                <i className="bi bi-twitter"></i>
-            </a>
+            
         </div>
     )
 }
